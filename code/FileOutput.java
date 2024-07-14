b88acb5111bdf3a76aec272ac416e2039ce518c8
Md Akhlakul Islam <109545500+aishaker129@users.noreply.github.com>
2024-07-14 21:52:42 +0600

    Add files via upload

--- code/FileOutput.java
+++ code/FileOutput.java
@@ -5,10 +5,14 @@ public class FileOutput {
 
     public static void main(String[] args)throws Exception {
         try (FileInputStream fis = new FileInputStream("/home/rifatul/Desktop/code/sh.txt")){
-         byte b[] = new byte[fis.available()];
-         fis.read(b);
-         String str = new String(b);
-         System.out.println(str);
+//         byte b[] = new byte[fis.available()];
+//         fis.read(b);
+//         String str = new String(b);
+//         System.out.println(str);
+            int c;
+            while((c=fis.read())!=-1){
+                System.out.print((char)c);
+            }
         }
     }
 }
